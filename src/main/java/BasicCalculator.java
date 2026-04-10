74cd78b98984be6c38017a4d01461469e71592a5
killioin007-source <killioin.007@gmail.com>
2026-04-10 10:06:05 -0400

    Initial commit

---
+++ src/main/java/BasicCalculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class BasicCalculator {
+    public static void main(String[] args) {
+        //First number input
+        System.out.print("What's your first number?");
+        Scanner scanner1 = new Scanner(System.in);
+        int firstNumber = scanner1.nextInt();
+
+        //Second number input
+        System.out.print("What's your second number?");
+        Scanner scanner2 = new Scanner(System.in);
+        int secondNumber = scanner2.nextInt();
+
+        //getting input from user
+        System.out.println("Possible Calculations: \n (A)dd \n (S)ubstract \n (M)ultiply \n (D)ivide \n Please select an option: ");
+        Scanner scanner3 = new Scanner(System.in);
+        String option = scanner3.nextLine();
+        if (option.equals("A")){
+            System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber) );
+        }
+
+        if (option.equals("S")){
+            System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber) );
+        }
+        if (option.equals("M")){
+            System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber) );
+        }
+        if (option.equals("D")){
+            System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber) );
+        }
+
+
+
+
+    }
+
+}
